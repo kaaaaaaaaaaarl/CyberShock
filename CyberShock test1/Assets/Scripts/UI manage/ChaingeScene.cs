9d6698c5d63056b2aa8339639884af3fa14b4114
kaaaaaaaaaaarl <97940914+kaaaaaaaaaaarl@users.noreply.github.com>
2024-03-27 15:08:13 +0200

    new menu

---
+++ CyberShock test1/Assets/Scripts/UI manage/ChaingeScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChaingeScene : MonoBehaviour
+{
+    public string sceneName;
+    public string sceneName1;
+    public string sceneName2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChaingeScenes()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ChaingeScenes1()
+    {
+        SceneManager.LoadScene(sceneName1);
+    }
+    public void ChaingeScenes2()
+    {
+        SceneManager.LoadScene(sceneName2);
+    }
+}
---
+++ CyberShock test1/Assets/Scripts/UI manage/OnTriggerAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerAnimation : MonoBehaviour
+{
+    Animator m_Animator;
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+    public void Animation1(){
+m_Animator.ResetTrigger("Crouch"); 
+    }
+}
