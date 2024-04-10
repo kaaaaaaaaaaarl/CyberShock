8b0bbf295d850da2bbe98eb3141e6dfc884f0e59
kaaaaaaaaaaarl <97940914+kaaaaaaaaaaarl@users.noreply.github.com>
2024-04-11 01:29:57 +0300

    Volume controll added
    I added 3 new settings

---
+++ CyberShock test1/Assets/Scripts/Options/Volume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Volume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Main", volume);
+    }
+    public void SetSfx (float volume)
+    {
+        audioMixer.SetFloat("SFX", volume);
+    }
+    public void SetMusic (float volume)
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+}
