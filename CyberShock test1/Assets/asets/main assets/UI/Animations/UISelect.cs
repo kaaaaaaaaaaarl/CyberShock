0cd5161e7f1cfa77db3cb66f724ea74810498603
kaaaaaaaaaaarl <97940914+kaaaaaaaaaaarl@users.noreply.github.com>
2024-04-25 20:04:25 +0300

    GUI update

---
+++ CyberShock test1/Assets/asets/main assets/UI/Animations/UISelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelect : MonoBehaviour
+{
+    public Animator animator;
+    public bool value;
+    public void Load(bool value){
+        animator.SetBool("Selected", value);
+    }
+    void Start()
+    {
+        
+    }
+}
